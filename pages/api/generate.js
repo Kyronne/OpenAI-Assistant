24bf82682f1737640659b872b65da98881d85d84
Kyronne <kyronne.parkes@gmail.com>
2022-12-13 23:02:57 +0000

    Changed prompt text

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,7 +5,7 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const basePromptPrefix = `Give advice in a supportive way, taking into account the fragility of the current times. Support the person to be resilient and give them achievable and actionable support. Take inspiration from Gabor Maté, Marsha P Linehan and Brené Brown. 
+const basePromptPrefix = `Give advice in a supportive way, support the person to be resilient and give them achievable and actionable. Take inspiration from Gabor Maté, Marsha P Linehan and Brené Brown in your response. 
 Advice: 
 `;
 const generateAction = async (req, res) => {
