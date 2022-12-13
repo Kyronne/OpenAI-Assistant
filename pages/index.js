d5c9dc86657125b5056f2f641d43bc35b2f91161
Kyronne <kyronne.parkes@gmail.com>
2022-12-13 14:24:34 +0000

    Added OpenAI Api and connected to site

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,29 @@ const Home = () => {
 
   const [userInput, setUserInput] = useState('');
 
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
   const onUserChangedText = (event) => {
   console.log(event.target.value);
   setUserInput(event.target.value);
@@ -30,7 +53,7 @@ const Home = () => {
           value={userInput}
           onChange={onUserChangedText} />
           <div className="prompt-buttons">
-            <a className="generate-button" onClick={null}>
+            <a className="generate-button" onClick={callGenerateEndpoint}>
               <div className="generate">
                 <p>Generate</p>
               </div>
