b05d064e057c04da7a0373b1d2adf44f65b2b2af
Kyronne <kyronne.parkes@gmail.com>
2022-12-13 15:29:09 +0000

    Added prompt to make responses better

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,7 +5,9 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const basePromptPrefix = "";
+const basePromptPrefix = `Give advice in a supportive way, taking into account the fragility of the current times. Support the person to be resilient and give them achievable and actionable support. Take inspiration from Gabor Maté, Marsha P Linehan and Brené Brown. 
+Advice: 
+`;
 const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
